abc77770d1391a1d61ef74d26465ac22430edc03
Frank Chen <frankcrestview@hotmail.com>
2019-05-13 19:24:02 -0400

    Initial Commit

---
+++ Ratings1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ratings1
+//
+//  Created by Frank Chen on 2019-05-13.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ratings1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ratings1
+//
+//  Created by Frank Chen on 2019-05-13.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
